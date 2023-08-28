14d6fe2ae109464989d008af099dda27df7897b8
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-08-28 20:55:24 -0300

    feat: fetch api tests

--- frontend/src/__tests__/modules/home/Home.spec.tsx
+++ frontend/src/__tests__/modules/home/Home.spec.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import {Home} from '../../../modules/home';
+import {rest} from "msw";
+import {setupServer} from "msw/node";
 
+
+const server = setupServer(
+    rest.get('https://rickandmortyapi.com/api/character', (req, res, ctx) => {
+        return res(ctx.json({results: [{id: 1, name: 'Rick'}]}));
+    })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('displays character names after loading', async () => {
+    render(<Home/>);
+
+    // Aguarde até que os dados sejam carregados e os personagens sejam exibidos
+    await screen.findByText('Rick');
+
+    const characterName = screen.getByText('Rick');
+    expect(characterName).toBeInTheDocument();
+});
 test('displays loading indicator on page load', () => {
     render(<Home/>);
 
@@ -10,7 +32,7 @@ test('displays loading indicator on page load', () => {
 });
 
 test('displays loading indicator on page refresh', () => {
-    render(<Home />);
+    render(<Home/>);
 
     const loadingIndicator = screen.getByTestId('loading-indicator');
     expect(loadingIndicator).toBeInTheDocument();
--- frontend/src/modules/home/index.tsx
+++ frontend/src/modules/home/index.tsx
@@ -1,27 +1,53 @@
 import React, {useEffect, useState} from 'react';
-import {Box, Container, Text} from "@chakra-ui/react";
+import {Box, Container, Text, Image, SimpleGrid} from "@chakra-ui/react";
+
+interface Character {
+    id: string;
+    name: string;
+    image: string;
+    species: string;
+    gender: string
+}
 
 export const Home = () => {
     const [isLoading, setIsLoading] = useState(true);
+    const [characters, setCharacters] = useState<Character[]>();
 
     useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 1000);
+        fetch('https://rickandmortyapi.com/api/character')
+            .then(response => response.json())
+            .then(data => {
+                setCharacters(data.results);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching characters:', error);
+                setIsLoading(false);
+            });
     }, []);
+    console.log(characters)
     return (
         <Container>
-            <Text >
-                Welcome to the Home Page
-            </Text>
+            <Text>Welcome to the Home Page</Text>
             <Box>
                 {isLoading ? (
                     <Box data-testid="loading-indicator">Loading...</Box>
                 ) : (
-                    // Conteúdo da página após o carregamento
-                    <Box data-testid={"content-after-loading"}>Content loaded!</Box>
+                    <SimpleGrid columns={{base: 1, md: 2, lg: 3}} gap={4}>
+                        {(characters || []).map((character: Character) => (
+                            <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p={4}>
+                                <Image src={character.image} alt={character.name}/>
+                                <Text mt={2} fontWeight="semibold" fontSize="xl">
+                                    {character.name}
+                                </Text>
+                                <Text>{character.species}</Text>
+                                <Text>{character.gender}</Text>
+                            </Box>
+                        ))}
+                    </SimpleGrid>
                 )}
             </Box>
         </Container>
     )
-}
+        ;
+};
